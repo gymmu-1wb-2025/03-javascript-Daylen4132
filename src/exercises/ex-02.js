2fc02c9f9eb2ec3cb2a8e785af5ed2e4de8e5e06
Daylen4132 <e273164@edu.sbl.ch>
2026-01-12 09:26:35 +0100

    WIP: Aufgabe 3

--- src/exercises/ex-02.js
+++ src/exercises/ex-02.js
@@ -1,4 +1,6 @@
 const myname = process.argv[2];
-const myage = process.argv[3];
+const myage = Number(process.argv[3]);
+
 
 console.log(`Hallo ${myname} ein alter ist ${myage}.`);
+console.log(`An der Matur wirst du ${myage + 4} Jahre alt sein.`);
