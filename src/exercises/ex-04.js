2bfd7ac78c09d0b0a01b81d20f0d5fe1d7ab8716
Daylen4132 <e273164@edu.sbl.ch>
2026-01-12 09:55:07 +0100

    3 arguments

--- src/exercises/ex-04.js
+++ src/exercises/ex-04.js
@@ -1,5 +1,5 @@
 const myweight = Number(process.argv[2]);
 const myheight = Number(process.argv[3]);
-
+const bmi = (myweight / (myheight * myheight)).toFixed(2);
 
 console.log(`Dein BMI ist ${bmi}.`);
