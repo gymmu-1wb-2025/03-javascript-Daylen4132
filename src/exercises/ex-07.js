22e8949ddf20c639206bf0d7b9da42d4c1a1cd47
Daylen4132 <e273164@edu.sbl.ch>
2026-01-23 09:01:46 +0100

    change ex.07.js

--- src/exercises/ex-07.js
+++ src/exercises/ex-07.js
@@ -1,2 +1,8 @@
 const mynumber = Number(process.argv[2]);
 const remainder = mynumber % Number(process.argv[3]);
+
+if (remainder === 0) {
+  console.log("The number is divisible by [" + Number(process.argv[3]) + "].");
+} else {
+  console.log("The number is not divisible by [" + Number(process.argv[3]) + "].");
+}
