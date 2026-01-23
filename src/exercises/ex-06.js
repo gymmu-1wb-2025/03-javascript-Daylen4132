9dff8e95442d483869d9abb0c0d0a4934b4bd622
Daylen4132 <e273164@edu.sbl.ch>
2026-01-23 08:38:12 +0100

    ex-06.js done

--- src/exercises/ex-06.js
+++ src/exercises/ex-06.js
@@ -1,4 +1,8 @@
 const mynumber = Number(process.argv[2]);
 const remainder = mynumber % 3;
 
-if (remainder)
+if (remainder === 0) {
+  console.log("The number is divisible by 3.");
+} else {
+  console.log("The number is not divisible by 3.");
+}
