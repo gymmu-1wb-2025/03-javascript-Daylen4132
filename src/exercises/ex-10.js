2998b5dd7a47eb82846dce75db68a4e1f033077d
Daylen4132 <e273164@edu.sbl.ch>
2026-01-30 09:09:30 +0100

    ex-10.js done

---
+++ src/exercises/ex-10.js
@@ -0,0 +1,6 @@
+console.log(`Du hast ${process.argv.length} Kommandozeilenargumente`);
+
+
+for(let i = 0; i < process.argv.length; i++) {
+    console.log(`Du hast ${process.argv[i]} Kommandozeilenargumente`);
+}
